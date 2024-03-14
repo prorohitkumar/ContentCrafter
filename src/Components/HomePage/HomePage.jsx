a7d7d79c642f57f2abaa44a00b8f4c7ff67cc73a
keshav <keshav@stackroute.in>
2024-03-14 15:39:07 +0530

    increase button size of carousel

--- src/Components/HomePage/HomePage.jsx
+++ src/Components/HomePage/HomePage.jsx
@@ -66,19 +66,20 @@ export default function HomePage() {
       ];
      
   return (
-    <div id="carouselExampleControls" style={{marginTop: '280px',}} className="carousel slide" data-bs-ride="carousel" ref={carouselRef}>
+  
+    <div id="carouselExampleControls" style={{marginTop: '270px',}} className="carousel slide" data-bs-ride="carousel" ref={carouselRef}>
       <div className="carousel-inner">
         <div className="carousel-item active">
           <div className="container">
             <div className="row justify-content-center">
               {cardDetails.slice(0, 4).map((card, index) => (
-                <div className="col-lg-3 col-md-6 mb-4" key={index}>
+                <div className="col-lg-3 col-md-6 mb-6" key={index}>
                   <div className="card h-100 card-hover">
                     <img src={card.imageSrc} className="card-img-top" alt="Card" style={{ height: '300px', objectFit: 'cover' }} />
                     <div className="card-body d-flex flex-column">
                       <h5 className="card-title">{card.title}</h5>
                       <p className="card-text">{card.text}</p>
-                      <a href={card.link} className="btn btn-primary mt-auto" style={{backgroundColor: '#0061af',marginLeft:'30px',marginRight:'30px'}}>
+                      <a href={card.link} className="btn btn-primary mt-auto" style={{backgroundColor: '#2980b9',marginLeft:'30px',marginRight:'30px'}}>
                   Start
                 </a>
                     </div>
@@ -92,13 +93,13 @@ export default function HomePage() {
           <div className="container">
             <div className="row">
               {cardDetails.slice(4).map((card, index) => (
-                <div className="col-lg-3 col-md-6 mb-4" key={index}>
+                <div className="col-lg-3 col-md-6 mb-6" key={index}>
                   <div className="card h-100 card-hover">
                     <img src={card.imageSrc} className="card-img-top" alt="Card" style={{ height: '300px', objectFit: 'cover' }} />
                     <div className="card-body d-flex flex-column">
                       <h5 className="card-title">{card.title}</h5>
                       <p className="card-text">{card.text}</p>
-                      <a href={card.link} className="btn btn-primary mt-auto" style={{backgroundColor: '#0061af',marginLeft:'30px',marginRight:'30px'}}>
+                      <a href={card.link} className="btn btn-primary mt-auto" style={{backgroundColor: '#2980b9',marginLeft:'30px',marginRight:'30px'}}>
                   Start
                 </a>
                     </div>
@@ -109,12 +110,12 @@ export default function HomePage() {
           </div>
         </div>
       </div>
-      <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev" style={{marginTop: '13%'}}>
-        <span className="carousel-control-prev-icon" ></span>
+      <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev" style={{marginTop: '12%'}}>
+        <span className="carousel-control-prev-icon fontIcon" ></span>
         {/* <span>Previous</span> */}
       </button>
-      <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next" style={{marginTop: '13%'}}>
-        <span className="carousel-control-next-icon"></span>
+      <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next" style={{marginTop: '12%'}}>
+        <span className="carousel-control-next-icon fontIcon"></span>
         {/* <span className="visually-hidden">Next</span> */}
       </button>
         
